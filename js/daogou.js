bbeb6ca7963b569c4f31d40636328ad08a29e804
wyw <645507046@qq.com>
2022-04-08 18:49:47 +0800

    '整合2'

---
+++ js/daogou.js
@@ -0,0 +1,8 @@
+$(function () {
+    $(function () {
+        $('.coolsm').on('click', function () {
+            $(this).addClass('chred').siblings().removeClass('chred');
+        })
+
+    })
+})
